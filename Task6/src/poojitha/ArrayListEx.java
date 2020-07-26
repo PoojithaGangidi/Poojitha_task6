f04acafa4294ec2e3c148eed394051d2c2fe08d7
PoojithaGangidi <poojithareddy.9051@gmail.com>
2020-07-26 16:07:46 +0530

    commit

---
+++ Task6/src/poojitha/ArrayListEx.java
@@ -0,0 +1,15 @@
+package poojitha;
+import java.util.*;
+public class ArrayListEx {
+	 @SuppressWarnings({ "rawtypes", "unchecked" })
+	 public static void main(String args[]){
+	   
+	  List al=new ArrayList();  // creating array list
+	  al.add("Jack");                // adding elements    
+	  al.add("Tyler");
+	  Iterator itr=al.iterator();
+	  while(itr.hasNext()){
+	  System.out.println(itr.next());
+	  }
+	  }
+}
